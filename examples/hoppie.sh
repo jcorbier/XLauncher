9cbc30fa7a008412d5a0c40ad3b84d8a979dab1e
Jeremie Corbier <jeremie.corbier@gmail.com>
2026-08-11 16:25:53 +0200

    fix: use a dedicated env variable for the network name in hoppie.sh
    This allows to use profiles with different names (Aircraft, GA, etc) and still
    be able to update which network is used with the script
    
    Signed-off-by: Jeremie Corbier <jeremie.corbier@gmail.com>

--- examples/hoppie.sh
+++ examples/hoppie.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 # Send POST request to Hoppie
-# network is set to the current XLauncher profile
-# profile names should therefore be VATSIM, IVAO, etc.
-# also expects EMAIL and HOPPIE_LOGON variables to be defined in Settings
-curl -d "email=${EMAIL}&logon=${HOPPIE_LOGON}&network=${XLAUNCHER_PROFILE}" \
+curl -d "email=${EMAIL}&logon=${HOPPIE_LOGON}&network=${NETWORK}" \
      -X POST https://www.hoppie.nl/acars/system/account.html 2>&1 >/dev/null
